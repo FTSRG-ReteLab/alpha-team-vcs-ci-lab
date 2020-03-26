caedb3643df5ba1828a72d6947e4a2cebb26104d
DaneeF <finta.daniel.j@gmail.com>
2020-03-26 12:30:39 +0100

    Reference speed implementation added

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,12 +2,27 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-
+	private Timer timer;
+	
+	public TrainControllerImpl() {
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("TrainControllerImpl timer running");
+				followSpeed();
+			}
+		}, 1000, 500);
+	}
+	
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
